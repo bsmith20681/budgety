e809cbbfeb456138f9ac641a90df3533444c5c1a
Test <bsmith20681@gmail.com>
2019-02-12 18:21:48 -0700

    added delete from UI

--- app.js
+++ app.js
@@ -64,6 +64,20 @@ var budgetController = (function(){
 
     },
 
+    deleteItem: function(type, id) {
+      var ids, index;
+
+      ids = data.allItems[type].map(function(current){
+        return current.id;
+      });
+
+      index = ids.indexOf(id);
+
+      if (index !== -1) {
+        data.allItems[type].splice(index, 1);
+      }
+    },
+
     calculateBudget: function() {
 
       calculateTotal('exp');
@@ -137,6 +151,12 @@ var UIController = (function() {
       document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
     },
 
+    deleteListItem: function(selectorID) {
+      var el = document.getElementById(selectorID)
+
+      el.parentNode.removeChild(el);
+    },
+
     clearFields: function() {
       var fields, fieldsArr;
 
@@ -196,6 +216,10 @@ var controller = (function(budgetCtrl, UICtrl) {
 
   };
 
+  var updatePercentages = function() {
+
+  };
+
   var ctrlAddItem = function() {
     var input, newItem;
 
@@ -209,11 +233,28 @@ var controller = (function(budgetCtrl, UICtrl) {
       UICtrl.clearFields();
 
       updateBudget();
+
+      updatePercentages();
     }
   };
 
   var ctrlDeleteItem = function(event) {
-    
+    var itemID, splitID, type, ID;
+
+    itemID = event.target.parentNode.parentNode.parentNode.parentNode.id;
+
+    if(itemID) {
+
+      splitID = itemID.split('-');
+      type = splitID[0];
+      ID = parseInt(splitID[1]);
+
+      budgetCtrl.deleteItem(type, ID);
+      UICtrl.deleteListItem(itemID);
+      updateBudget();
+      updatePercentages();
+    }
+
   };
 
   return {
